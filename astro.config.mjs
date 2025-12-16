cd43eb45b3100995ae254262ffe0ce5b167dec3d
25okapy <ookakosuke2@gmail.com>
2025-12-16 14:41:50 +0900

    Add Cloudflare adapter for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: cloudflare(),
 });
